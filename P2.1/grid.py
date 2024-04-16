965777c42a55d86f6ba5ff9dea597a64a4be4171
anaezquerro <anaezquerro0701@gmail.com>
2024-04-16 09:22:28 +0200

    p2.2

---
+++ P2.1/grid.py
@@ -0,0 +1,49 @@
+from itertools import product 
+from collections import OrderedDict
+import pandas as pd
+from keras.initializers import *
+from keras.optimizers import * 
+from keras.regularizers import * 
+from keras.layers import *
+from model import WalmartModel
+
+from utils import * 
+from data import * 
+
+Regularizer.__str__ = lambda x: str(x.__class__.__name__)
+Optimizer.__str__ = lambda x: str(x.__class__.__name__) + f'({float(x.learning_rate.numpy()):1.0e})'
+
+# global parameters 
+TEST_RATIO = 0.2
+VAL_RATIO = 0.15
+BATCH_SIZE = 200
+
+# load data 
+data = WalmartDataset.load('Walmart.csv')
+train, val, test = data.split(VAL_RATIO, TEST_RATIO)
+
+if __name__ == '__main__':
+    
+    grid = OrderedDict(
+        regularizer = [L1(1e-3), L2(1e-3), L1L2(1e-4)],
+        initializer=['random_normal', 'glorot_uniform'],
+        activation=['tanh', 'relu']
+    )
+
+    def applydeep(lists, func):
+        result = []
+        for item in lists:
+            result.append(list(map(func, item)))
+        return result
+
+    df = pd.DataFrame(columns=['train', 'val', 'test'], index=pd.MultiIndex.from_product(applydeep(grid.values(), str)))
+    for i, params in enumerate(product(*grid.values())):
+        if i < 5:
+            continue 
+        params = dict(zip(grid.keys(), params))
+        model = WalmartModel(seq_len=3, base_layer=GRU, num_encoder_layers=3, num_decoder_layers=2,  bidirectional=False, dropout=0.1,**params)
+        model.train(train, test, f'results/walmart3.weights.h5', Adam(1e-3), batch_size=BATCH_SIZE)
+        (_, train_mae, _), (_, val_mae, _), (_, test_mae, _) = map(model.evaluate, (train, val, test))
+        df.loc[tuple(map(str, params.values()))] = [train_mae, val_mae, test_mae]
+        df.to_csv('grid3.csv')
+    df.index.names = grid.keys()
---
+++ P2.2/model.py
@@ -0,0 +1,82 @@
+from keras.models import Sequential 
+from keras.layers import GRU, LSTM, Bidirectional, Dense, Dropout, Embedding, SimpleRNN, Layer
+from keras.optimizers import Optimizer, Adam
+from keras.regularizers import Regularizer, L1, L2, L1L2
+from typing import Union, List, Optional, Callable, Tuple 
+import plotly.graph_objects as go
+from data import AmazonDataset
+from keras.callbacks import EarlyStopping, History, ModelCheckpoint
+from utils import plot_history
+import numpy as np 
+
+class AmazonReviewsModel:
+    def __init__(
+        self,
+        vocab_size: int,
+        embed_size: int,
+        recurrent_layer: Layer,
+        num_recurrent_layers: int = 2,
+        recurrent_dims: Optional[Union[List[int], int]] = None,
+        ffn_dims: List[int] = [],
+        bidirectional: bool = False,
+        dropout: float = 0.0,
+        activation: str = 'relu',
+        regularizer: Optional[Regularizer] = None, 
+        initializer: str = 'glorot_uniform',
+        name: str = 'AmazonReviewsModel'
+    ):
+        if recurrent_dims is None:
+            recurrent_dims = embed_size
+        if isinstance(recurrent_dims, int):
+            recurrent_dims = [recurrent_dims for _ in range(num_recurrent_layers)]
+        
+        if bidirectional:
+            add_layer = lambda *args, **kwargs: Bidirectional(recurrent_layer(*args, **kwargs))
+        else:
+            add_layer = recurrent_layer
+            
+        
+        self.model = Sequential([
+            Embedding(input_dim=vocab_size, output_dim=embed_size, mask_zero=True),
+            
+            # recurrent encoder
+            *[add_layer(dim, kernel_regularizer=regularizer, kernel_initializer=initializer, return_sequences=True) for dim in recurrent_dims[:-1]],
+            add_layer(recurrent_dims[-1]),
+            Dropout(dropout),
+            
+            # ffn decoder 
+            *[Dense(dim, activation=activation) for dim in ffn_dims],
+            Dense(1, activation='sigmoid')
+        ], name=name)
+        
+    def train(
+        self,
+        data: AmazonDataset,
+        path: str,
+        lr: float = 1e-3,
+        opt: Callable = Adam,
+        epochs: int = 10, 
+        batch_size: int = 30, 
+        dev_patience: int = 10,
+        train_patience: int = 5,
+        clipnorm: Optional[int] = None,
+        clipvalue: Optional[int] = None
+    ) -> Tuple[History, go.Figure]:
+        optimizer = opt(lr, clipnorm=clipnorm, clipvalue=clipvalue)
+        callbacks = [
+            EarlyStopping(monitor="val_loss", patience=dev_patience, mode="min", verbose=0),
+            EarlyStopping(monitor="loss", patience=train_patience, mode="min", verbose=0),
+            ModelCheckpoint(path, monitor="val_loss", mode="min", save_best_only=True, save_weights_only=True, verbose=0),
+        ]
+        self.model.compile(optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+        history = self.model.fit(
+            data.X_train, data.y_train, epochs=epochs, batch_size=batch_size, callbacks=callbacks,
+            validation_data=(data.X_val, data.y_val)
+        )
+        self.model.load_weights(path)
+        return history, plot_history(history.history, metrics=['loss', 'accuracy'], name=self.model.name)
+    
+    def evaluate(self, inputs: np.ndarray, targets: np.ndarray, batch_size: int = 1000):
+        return self.model.evaluate(inputs, targets, verbose=0, batch_size=batch_size)
+        
+    
