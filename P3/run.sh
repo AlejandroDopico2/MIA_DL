fcf61a2b34724050e092b033ce605e6a4557fa77
anaezquerro <anaezquerro0701@gmail.com>
2024-05-26 22:59:51 +0200

    renamed

--- P3/run.sh
+++ P3/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python3 main.py wgan -p wgan --hidden-size 512 --batch-size 20
+CUDA_VISIBLE_DEVICES=1 python3 main.py wgan -p wgan --hidden-size 512 --batch-size 100
